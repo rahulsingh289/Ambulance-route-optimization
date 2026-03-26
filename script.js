d44c070ed37b23cda8e76947d3c3f7d96fa5779f
Rohitkumar0401 <rohitdhyani17@gmail.com>
2026-03-26 15:33:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function login(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let message = document.getElementById("message");
+
+    if(email === "" || password === ""){
+        message.innerHTML = "⚠ Please fill all fields";
+        message.style.color = "yellow";
+        return;
+    }
+
+    // Demo login (connect with backend later)
+    if(email === "admin@gmail.com" && password === "1234"){
+        message.innerHTML = "✅ Login Successful!";
+        message.style.color = "lightgreen";
+
+        setTimeout(()=>{
+            alert("Redirecting to Dashboard...");
+        },1000);
+    } else {
+        message.innerHTML = "❌ Invalid credentials";
+        message.style.color = "red";
+    }
+}
